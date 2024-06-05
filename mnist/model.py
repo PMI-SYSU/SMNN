6fe6d0a963d6b9922921dafc1ffc91eb9ee116e8
LIN ZHANGHAN <133510844+LinZhanghan@users.noreply.github.com>
2024-06-05 16:15:08 +0800

    Add files via upload

--- mnist/model.py
+++ mnist/model.py
@@ -1,16 +1,21 @@
 import torch
 import torch.nn as nn
+import snntorch as snn
+import torch.nn.functional as F
+import numpy as np
+from torch.nn import init
 from model_settings import *
 
+# Define Network
 class MDL_RNN_mnist(nn.Module):
 
     """
 
-    MDL RNN model for mnist task
+    MDL spiking model for mnist task
 
     """
 
-    def __init__(self,input_shape, hidden_shape,output_shape,P,filter='single'):
+    def __init__(self,input_shape, hidden_shape,output_shape,P,filter='double',mdl=1,tref=2,replace=0):
 
         """
 
@@ -19,33 +24,46 @@ class MDL_RNN_mnist(nn.Module):
         num_outputs: output size
         filter: synaptic filter
         P: mode size
-        Win: input matrix
-        Wout: output matrix
+        Win: input weight
+        Wout: output weight
         pin: input mode
         pout: output mode
         l: connectivity importance
+        mdl: flag for using mdl
+        tref: refractory period
+        replace: flag for replacing step function
+        Wr : connectivity matrix for standard fully connection
 
-        """    
+        """   
 
-        super().__init__()
-        self.num_inputs = input_shape
-        self.num_hidden = hidden_shape
-        self.num_outputs = output_shape
+        self.input_shape = input_shape
+        self.hidden_shape = hidden_shape
+        self.output_shape = output_shape
         self.filter=filter
         self.P=P
         self.Win=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.input_shape)),requires_grad=True)
-        self.Wout=torch.nn.Parameter(torch.randn(size=(self.output_shape,self.hidden_shape)),requires_grad=True)
-        self.pin=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
-        self.pout=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
-        self.l=torch.nn.Parameter(torch.zeros(size=[self.P]),requires_grad=True)
+        self.Wout=torch.nn.Parameter(torch.randn(size=(self.output_shape,self.hidden_shape)),requires_grad=False)
+        self.tref=tref
+        self.mdl=mdl
+        self.replace=replace
+        if mdl:    
+            self.pin=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
+            self.pout=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
+            self.l=torch.nn.Parameter(torch.randn(size=[self.P]),requires_grad=True)
+        else:
+            self.Wr=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.hidden_shape)),requires_grad=True)
 
-    def forward(self, x):
+    def forward(self, inputs):
 
         """
 
         Forward dynamics for model
 
-        x: input image
+        inputs: input image
+        mem: membrane potential for coding
+        r: storage variable for filtered firing rates
+        s: storage variable for spikes
+        spk_in: input spike train
         Wr: connectivity matrix
         U: membrane potential of hidden neuron
         S: storage list for spike train
@@ -53,65 +71,67 @@ class MDL_RNN_mnist(nn.Module):
         rm: the maximum value of firing rate
         
         Returns 
-            softmax(Wout * rm)
+            softmax(Wout * Um + Bias)
 
         """
+        
+        # Construct connectivity matrix
+        if self.mdl:
+            Wr=torch.matmul(self.l*self.pin,self.pout.T).to(device)
+        else:
+            Wr=self.Wr.to(device)
 
+        # Initialize variables
         U=[]
         S=[]
         R=[]
-        batch_size=x.shape[0]
-        
-        vthr=torch.tensor(1.0)
-        taus=torch.tensor(10e-3)
-        taum=torch.tensor(20e-3)
-        taud=torch.tensor(30e-3)
-        ls=torch.exp(-dt/taus).to(device)
-        lm=torch.exp(-dt/taum).to(device)
-        ld=torch.exp(-dt/taud).to(device)
-        tlast=torch.zeros(size=(batch_size,self.hidden_shape,1)).to(device)-1
-        tref=5*dt
+        batch_size=inputs.shape[0]
+        spk_in = spikegen.rate(inputs.flatten(1),T).reshape(T,-1,784,1)
         if self.filter=='double': 
-            taur=torch.tensor(2e-3)
-            lr=torch.exp(-dt/taur).to(device)
+            tau_r=torch.tensor(2)
+            lr=torch.exp(-dt/tau_r).to(device)
             h = torch.zeros(size=(self.hidden_shape,1)).to(device)
-            
-        spk_in = spikegen.rate(x.flatten(1),time_steps).reshape(time_steps,-1,784,1)    
-        
-        Wr=torch.matmul(self.l*self.pin,self.pout.T).to(device)
-        
+        tref=self.tref
+        tlast=torch.zeros(size=(batch_size,self.hidden_shape,1)).to(device)-1
         I = torch.zeros(size=(batch_size,self.hidden_shape,1)).to(device)
         mem = torch.zeros(size=(batch_size,self.hidden_shape,1)).to(device)
         s = torch.zeros(size=(batch_size,self.hidden_shape,1)).to(device)
+        lm=torch.exp(-dt/taum).to(device)
+        ld=torch.exp(-dt/tau_d).to(device)
         r = torch.zeros(size=(self.hidden_shape,1)).to(device)
-        
-        for i in range(time_steps):
-            I=ls*I+torch.matmul(self.Win,spk_in[i])+torch.matmul(Wr,r)
+
+        #Dynamics
+        for i in range(T):
+            I=torch.matmul(self.Win,spk_in[i])+torch.matmul(Wr,r)
             mem=(dt*i>(tlast+tref))*(lm*mem+(1-lm)*I)*(1-s)
             if self.filter=='single':
-                r=torch.exp(-dt/tau_d)*r+dt/tau_d*s  
+                r=ld*r+dt/tau_d*s  
             elif self.filter=='double':
                 h=lr*h+dt/tau_d/tau_r*s
                 r=ld*r+dt*h  
-            s=spike_grad(mem-vthr)
+            if self.replace:
+                s=spike_grad_(mem-vthr)
+            else:
+                s=spike_grad(mem-vthr)
             tlast=tlast+(dt*i-tlast)*s
             U.append(mem)
             S.append(s)
             R.append(r)
-            
         S=torch.stack(S,0)
         U=torch.stack(U,0)
         R=torch.stack(R,0)
         rm,_=torch.max(R,0)
-        rout=torch.matmul(self.Wout,rm)
-        y = torch.softmax(rout,1)
         self.U=U
+        self.inputs=inputs
         self.spk=spk_in
         self.S=S
         self.R=R
         self.rm=rm
-        
-        return y
+
+        #Generate output
+        rout=torch.matmul(self.Wout,rm)
+        output = torch.softmax(rout,1)
+        return output
     
     def initialize(self):
 
@@ -121,98 +141,115 @@ class MDL_RNN_mnist(nn.Module):
 
         """
         
-        for name, param in model.named_parameters():
+        for name, param in self.named_parameters():
             if 'Win' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(input_shape))
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.hidden_shape))
             elif 'pin' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(P))
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.P))
             elif 'pout' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(P))
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.P))
             elif 'l' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(hidden_shape))
+                _=init.normal_(param, mean=0, std=np.sqrt(self.P)/np.sqrt(self.hidden_shape))
             elif 'Wout' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(output_shape))
-
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.hidden_shape))
+            elif 'Wr' in name:
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.hidden_shape))
 
 class rate_RNN_mnist(nn.Module):
+    
+    """
+
+    MDL rate model for mnist task
 
     """
 
-    rate RNN model for mnist task
-
-    """
-
-    def __init__(self,input_shape, hidden_shape,output_shape,P):
+    def __init__(self,input_shape, hidden_shape,output_shape,P,mdl=1):
 
         """
-
+        
         num_inputs: input size
         num_hidden: number of hidden neurons
         num_outputs: output size
         P: mode size
-        Win: input matrix
-        Wout: output matrix
+        Win: input weight
+        Wout: output weight
         pin: input mode
         pout: output mode
         l: connectivity importance
+        mdl: flag for using mdl
+        Wr : connectivity matrix for standard fully connection
 
-        """    
+        """      
 
         super().__init__()
-        self.num_inputs = input_shape
-        self.num_hidden = hidden_shape
-        self.num_outputs = output_shape
+        self.input_shape = input_shape
+        self.hidden_shape = hidden_shape
+        self.output_shape = output_shape
         self.P=P
         self.Win=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.input_shape)),requires_grad=True)
         self.Wout=torch.nn.Parameter(torch.randn(size=(self.output_shape,self.hidden_shape)),requires_grad=True)
-        self.pin=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
-        self.pout=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
-        self.l=torch.nn.Parameter(torch.zeros(size=[self.P]),requires_grad=True)
+        self.pin=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)#输入模式
+        self.pout=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)#输出模式
+        self.l=torch.nn.Parameter(torch.zeros(size=[self.P])+1/self.hidden_shape,requires_grad=False)#连接重要性
+        self.mdl=mdl
+        if mdl:    
+            self.pin=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
+            self.pout=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
+            self.l=torch.nn.Parameter(torch.randn(size=[self.P]),requires_grad=True)
+        else:
+            self.Wr=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.hidden_shape)),requires_grad=True)
 
-    def forward(self, x):
+    def forward(self, inputs):
 
         """
 
         Forward dynamics for model
 
-        x: input image
+        inputs: input image
+        mem: membrane potential for coding
+        r: storage variable for filtered firing rates
+        spk_in: input spike train
         Wr: connectivity matrix
         U: membrane potential of hidden neuron
         R: storage list for firing rate
         rm: the maximum value of firing rate
         
         Returns 
-            softmax(Wout * rm)
+            softmax(Wout * Um + Bias)
 
         """
 
-        U=[]
+        # Construct connectivity matrix
+        if self.mdl:
+            Wr=torch.matmul(self.l*self.pin,self.pout.T).to(device)
+        else:
+            Wr=self.Wr.to(device)
+
+        # Initialize variables
         R=[]
-        batch_size=x.shape[0]
-        
-        taum=torch.tensor(20e-3)
-        lm=torch.exp(-dt/taum).to(device)
-        
-        Wr=torch.matmul(self.l*self.pin,self.pout.T).to(device)
-        
-        spk_in = spikegen.rate(x.flatten(1),time_steps).reshape(time_steps,-1,784,1)
-        
+        U=[]
+        batch_size=inputs.shape[0]
+        spk_in = spikegen.rate(inputs.flatten(1),T).reshape(T,-1,784,1)
         mem = torch.zeros(size=(batch_size,self.hidden_shape,1)).to(device)
-        
-        for i in range(time_steps):
+        lm=torch.exp(-dt/tau_m).to(device)
+
+        #Dynamics
+        for i in range(T):
             I=torch.matmul(self.Win,spk_in[i])+torch.matmul(Wr,torch.tanh(mem))
             mem = lm*mem+(1-lm)*I
             U.append(mem)
             R.append(torch.tanh(mem))
-            
         U=torch.stack(U,0)
         R=torch.stack(R,0)
         rm,_=torch.max(R,0)
-        Rout=torch.matmul(self.Wout,Rm)
-        y = torch.softmax(Rout,1)
         self.R=R
         self.U=U
+        self.inputs=inputs
         self.spk=spk_in
+
+        #Generate output
+        rout=torch.matmul(self.Wout,rm)
+        output = torch.softmax(rout,1)
         return y
     
     def initialize(self):
@@ -223,15 +260,16 @@ class rate_RNN_mnist(nn.Module):
 
         """
         
-        for name, param in model.named_parameters():
+        for name, param in self.named_parameters():
             if 'Win' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(input_shape))
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.hidden_shape))
             elif 'pin' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(P))
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.P))
             elif 'pout' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(P))
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.P))
             elif 'l' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(hidden_shape))
+                _=init.normal_(param, mean=0, std=np.sqrt(self.P)/np.sqrt(self.hidden_shape))
             elif 'Wout' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(output_shape))
-
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.hidden_shape))
+            elif 'Wr' in name:
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.hidden_shape))
--- mnist/model_settings.py
+++ mnist/model_settings.py
@@ -6,6 +6,7 @@ from snntorch import surrogate
 """
 
 Model settings for mnist task
+
 transform: transform method for mnist images
 data_path: data path for mnist dataset
 P: mode size
@@ -13,18 +14,25 @@ hidden_shape: number of neurons
 input_shape: numbers of input signals
 output_shape: output size
 n: trials of training
-time_steps: time steps
+T: time steps
 batchsize: batch size for training
 device: cpu or gpu device for running
 spike_grad: surrogate delta function
-dt: time interval
+spike_grad_approx : smooth transfer function
+dt: time step
 train_loader: data loader for training
 test_loader: data loader for testing
 optimizer: optimizer
+vthr : firing threshold
+tau_m : membrane time constant
+tau_d : synaptic decay time constant
+tau_r : synaptic rise time constant
 
 """
 
 spike_grad = surrogate.fast_sigmoid()
+def spike_grad_approx(x,beta=20):
+    return (1+torch.tanh(beta*x))/2
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 # Define a transform
 transform = transforms.Compose([
@@ -33,10 +41,10 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0,), (1,))])
 data_path='./data/mnist'
-time_steps = 100
+T = 100
 input_shape=784
 num_classes=10
-dt=2e-3
+dt=0.2
 batch_size = 256
 n=10
 mnist_train = datasets.MNIST(data_path, train=True, download=True, transform=transform)
@@ -44,7 +52,10 @@ mnist_test = datasets.MNIST(data_path, train=False, download=True, transform=tra
 # Create DataLoaders
 train_loader = DataLoader(mnist_train, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(mnist_test, batch_size=500, shuffle=True)
-optimizer = torch.optim.Adam
+
 P=3
 hidden_shape=100
-
+optimizer = torch.optim.Adam
+vthr=torch.tensor(1.0)
+taum=torch.tensor(20)
+tau_d=torch.tensor(30)
