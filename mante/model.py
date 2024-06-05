0a9b3d7997cd152da8f44fbf9b66a39585a7a23e
LIN ZHANGHAN <133510844+LinZhanghan@users.noreply.github.com>
2024-06-05 16:15:36 +0800

    Add files via upload

--- mante/model.py
+++ mante/model.py
@@ -6,15 +6,16 @@ from torch.nn import init
 from model_settings import *
 
 
-class MDL_RNN_mante(nn.Module):
+# Define Network
+class MDL_RNN_mate(nn.Module):
 
     """
 
-    MDL RNN model for contextual-dependent task
+    MDL Spiking model for contextual-dependent task
 
     """
 
-    def __init__(self,input_shape,hidden_shape,output_shape,P):
+    def __init__(self,input_shape,hidden_shape,output_shape,P,filter='double',mdl=1,tref=2,replace=0):
 
         """
 
@@ -23,205 +24,113 @@ class MDL_RNN_mante(nn.Module):
         num_outputs: output size
         filter: synaptic filter
         P: mode size
-        Win: input matrix
-        Wout: output matrix
+        Win: input weight
+        Wout: output weight
         pin: input mode
         pout: output mode
         l: connectivity importance
+        mdl: flag for using mdl
+        tref: refractory period
+        replace: flag for replacing step function
+        Wr : connectivity matrix for standard fully connection
 
         """    
 
         super().__init__()
-        self.num_inputs = input_shape
-        self.num_hidden = hidden_shape
-        self.num_outputs = output_shape
-        self.filter=filter
+        self.input_shape = input_shape
+        self.hidden_shape = hidden_shape
+        self.output_shape = output_shape
         self.P=P
+        self.filter=filter
         self.Win=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.input_shape)),requires_grad=True)
         self.Wout=torch.nn.Parameter(torch.randn(size=(self.output_shape,self.hidden_shape)),requires_grad=True)
-        self.pin=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
-        self.pout=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
-        self.l=torch.nn.Parameter(torch.zeros(size=[self.P]),requires_grad=True)
+        self.mdl=mdl
+        self.tref=tref
+        self.replace=replace
+        if mdl:    
+            self.pin=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
+            self.pout=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
+            self.l=torch.nn.Parameter(torch.randn(size=[self.P]),requires_grad=True)
+        else:
+            self.Wr=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.hidden_shape)),requires_grad=True)
 
         
-    def forward(self, x):
+    def forward(self, inputs):
 
         """
 
         Forward dynamics for model
 
-        x: input image
+        mem: membrane potential
+        r: storage variable for filtered firing rates
+        s: storage variable for spikes
+        U: storage list for membrane potential
+        R: storage list for filtered firing rates
+        S: storage list s
+        I: external current
         Wr: connectivity matrix
-        U: membrane potential of hidden neuron
-        S: storage list for spike train
-        R: storage list for firing rate
-        rm: the maximum value of firing rate
         
         Returns
             matmul(W_out,R)
 
         """
 
+        # Construct connectivity matrix
+        if self.mdl:
+            Wr=torch.matmul(self.l*self.pin,self.pout.T).to(device)
+        else:
+            Wr=self.Wr.to(device)
+
+        # Initialize variables
         U=[]
         S=[]
         R=[]
-        if len(x.shape)>3:
-            batch_size=x.shape[1]
+        if len(inputs.shape)>3:
+            batch_size=inputs.shape[1]
         else:
             batch_size=1
-        time_steps=x.shape[0]
-        
-        vthr=torch.tensor(1.0)
-        taus=torch.tensor(10e-3)
-        taum=torch.tensor(20e-3)
-        tau_d=torch.tensor(30e-3)
-        ls=torch.exp(-dt/taus).to(device)
-        lm=torch.exp(-dt/taum).to(device)
-        ld=torch.exp(-dt/tau_d).to(device)
-        tlast=torch.zeros(size=(batch_size,self.hidden_shape,1)).to(device)-1
-        tref=5*dt
+        time_steps=inputs.shape[0]
         if self.filter=='double': 
-            tau_r=torch.tensor(2e-3)
             lr=torch.exp(-dt/tau_r).to(device)
             h = torch.zeros(size=(self.hidden_shape,1)).to(device)
-        
-        spk_in=x
-        
-        Wr=torch.matmul(self.l*self.pin,self.pout.T).to(device)
-        
+        tref=self.tref
+        tlast=torch.zeros(size=(batch_size,self.hidden_shape,1)).to(device)-1
         I = torch.zeros(size=(batch_size,self.hidden_shape,1)).to(device)
         mem = torch.zeros(size=(batch_size,self.hidden_shape,1)).to(device)
         s = torch.zeros(size=(batch_size,self.hidden_shape,1)).to(device)
+        lm=torch.exp(-dt/tau_m).to(device)
+        ld=torch.exp(-dt/tau_d).to(device)
         r = torch.zeros(size=(self.hidden_shape,1)).to(device)
-        
+
+        #Dynamics
         for i in range(time_steps):
-            I=ls*I+(torch.matmul(self.Win,spk_in[i])+torch.matmul(Wr,r))
+            I=torch.matmul(self.Win,inputs[i])+torch.matmul(Wr,r)
             mem=(dt*i>(tlast+tref))*(lm*mem+(1-lm)*I)*(1-s)
             if self.filter=='single':
                 r=ld*r+dt/tau_d*s  
             elif self.filter=='double':
                 h=lr*h+s
                 r=ld*r+(1-ld)*h  
-            s=spike_grad(mem-vthr)
+            if self.replace:
+                s=spike_grad_approx(mem-vthr)
+            else:
+                s=spike_grad(mem-vthr)
             tlast=tlast+(dt*i-tlast)*s
             U.append(mem)
             S.append(s)
             R.append(r)
-            
         S=torch.stack(S,0)
         U=torch.stack(U,0)
         R=torch.stack(R,0)
-        rout=torch.matmul(self.Wout,R)
-        y=rout
         self.U=U
-        self.spk=spk_in
+        self.inputs=inputs
         self.S=S
         self.R=R
-        return y
-
-    def initialize(self):
-
-        """
-
-        Method to initialize model parameters
-
-        """
         
-        for name, param in model.named_parameters():
-            if 'Win' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(input_shape))
-            elif 'pin' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(P))
-            elif 'pout' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(P))
-            elif 'l' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(hidden_shape))
-            elif 'Wout' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(output_shape))
-
-
-class rate_RNN_mante(nn.Module):
-
-    """
-
-    rate RNN model for contextual-dependent task
-
-    """
-
-    def __init__(self,input_shape, hidden_shape,output_shape,P):
-
-        """
-
-        num_inputs: input size
-        num_hidden: number of hidden neurons
-        num_outputs: output size
-        P: mode size
-        Win: input matrix
-        Wout: output matrix
-        pin: input mode
-        pout: output mode
-        l: connectivity importance
-
-        """    
-
-        super().__init__()
-        self.num_inputs = input_shape
-        self.num_hidden = hidden_shape
-        self.num_outputs = output_shape
-        self.P=P
-        self.Win=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.input_shape)),requires_grad=True)
-        self.Wout=torch.nn.Parameter(torch.randn(size=(self.output_shape,self.hidden_shape)),requires_grad=True)
-        self.pin=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
-        self.pout=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
-        self.l=torch.nn.Parameter(torch.zeros(size=[self.P]),requires_grad=True)
-
-    def forward(self, x):
-
-        """
-
-        Forward dynamics for model
-
-        x: input image
-        Wr: connectivity matrix
-        U: membrane potential of hidden neuron
-        R: storage list for firing rate
-        rm: the maximum value of firing rate
+        #Generate output
+        rout=torch.matmul(self.Wout,R)
+        return rout
         
-        Returns 
-            matmul(Wout,R)
-
-        """
-        U=[]
-        R=[]
-        if len(x.shape)>3:
-            batch_size=x.shape[1]
-        else:
-            batch_size=1
-        time_steps=x.shape[0]
-        
-        taum=torch.tensor(20e-3)
-        lm=torch.exp(-dt/taum).to(device)
-        
-        Wr=torch.matmul(self.l*self.pin,self.pout.T).to(device)
-        
-        spk_in = x
-        
-        mem = torch.zeros(size=(batch_size,self.hidden_shape,1)).to(device)
-        
-        for i in range(time_steps):
-            I=torch.matmul(self.Win,spk_in[i])+torch.matmul(Wr,torch.tanh(mem))
-            mem = lm*mem+(1-lm)*I
-            U.append(mem)
-            R.append(torch.tanh(mem))
-            
-        U=torch.stack(U,0)
-        R=torch.stack(R,0)
-        y=torch.matmul(self.Wout,R)
-        self.U=U
-        self.R=R
-        self.spk=spk_in
-        return y
-
     
     def initialize(self):
 
@@ -231,16 +140,133 @@ class rate_RNN_mante(nn.Module):
 
         """
         
-        for name, param in model.named_parameters():
+        for name, param in self.named_parameters():
             if 'Win' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(input_shape))
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.input_shape))
             elif 'pin' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(P))
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.P))
             elif 'pout' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(P))
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.P))
             elif 'l' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(hidden_shape))
+                _=init.normal_(param, mean=0, std=np.sqrt(self.P)/np.sqrt(self.hidden_shape))
             elif 'Wout' in name:
-                _=init.normal_(param, mean=0, std=1/np.sqrt(output_shape))
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.hidden_shape))
+            elif 'Wr' in name:
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.hidden_shape))
 
 
+class rate_RNN_mante(nn.Module):
+
+    """
+
+    MDL rate model for contextual-dependent task
+
+    """
+
+    def __init__(self,input_shape,hidden_shape,output_shape,P,mdl=1):
+        
+        """
+        
+        num_inputs: input size
+        num_hidden: number of hidden neurons
+        num_outputs: output size
+        P: mode size
+        Win: input weight
+        Wout: output weight
+        pin: input mode
+        pout: output mode
+        l: connectivity importance
+        mdl: flag for using mdl
+        Wr : connectivity matrix for standard fully connection
+
+        """            
+
+        super().__init__()
+        self.input_shape = input_shape
+        self.hidden_shape = hidden_shape
+        self.output_shape = output_shape
+        self.P=P
+        self.Win=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.input_shape)),requires_grad=True)
+        self.Wout=torch.nn.Parameter(torch.randn(size=(self.output_shape,self.hidden_shape)),requires_grad=True)
+        self.pin=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
+        self.pout=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
+        self.l=torch.nn.Parameter(torch.zeros(size=[self.P])+1/self.hidden_shape,requires_grad=False)
+        self.mdl=mdl
+        if mdl:    
+            self.pin=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
+            self.pout=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.P)),requires_grad=True)
+            self.l=torch.nn.Parameter(torch.randn(size=[self.P]),requires_grad=True)
+        else:
+            self.Wr=torch.nn.Parameter(torch.randn(size=(self.hidden_shape,self.hidden_shape)),requires_grad=True)
+
+    def forward(self, inputs):
+
+        """
+
+        Forward dynamics for model
+
+        mem: membrane potential
+        r: storage variable for filtered firing rates
+        U: storage list for membrane potential
+        R: storage list for filtered firing rates
+        I: external current
+        Wr: connectivity matrix
+        
+        Returns
+            matmul(W_out,R)
+
+        """
+        # Construct connectivity matrix    
+        if self.mdl:
+            Wr=torch.matmul(self.l*self.pin,self.pout.T).to(device)
+        else:
+            Wr=self.Wr.to(device)
+
+        # Initialize variables
+        R=[]
+        U=[]
+        if len(inputs.shape)>3:
+            batch_size=inputs.shape[1]
+        else:
+            batch_size=1
+        time_steps=inputs.shape[0]
+        mem = torch.zeros(size=(batch_size,self.hidden_shape,1)).to(device)
+        lm=torch.exp(-dt/tau_m).to(device)
+
+        #Dynamics
+        for i in range(time_steps):
+            I=torch.matmul(self.Win,inputs[i])+torch.matmul(Wr,torch.tanh(mem))
+            mem = lm*mem+(1-lm)*I
+            U.append(mem)
+            R.append(torch.tanh(mem))
+        U=torch.stack(U,0)
+        R=torch.stack(R,0)
+        self.U=U
+        self.R=R
+        self.inputs=inputs
+
+        #Generate output
+        rout=torch.matmul(self.Wout,R)
+        return rout
+
+    def initialize(self):
+
+        """
+
+        Method to initialize model parameters
+
+        """
+        
+        for name, param in self.named_parameters():
+            if 'Win' in name:
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.input_shape))
+            elif 'pin' in name:
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.P))
+            elif 'pout' in name:
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.P))
+            elif 'l' in name:
+                _=init.normal_(param, mean=0, std=np.sqrt(self.P)/np.sqrt(self.hidden_shape))
+            elif 'Wout' in name:
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.hidden_shape))
+            elif 'Wr' in name:
+                _=init.normal_(param, mean=0, std=1/np.sqrt(self.hidden_shape))
--- mante/model_settings.py
+++ mante/model_settings.py
@@ -15,26 +15,33 @@ zero_time1: zero time beform stimulus
 zero_time2: zero time after stimulus
 target_zero: target output of zero
 T: time steps
-dt: time internal
 zero1: zero input beform stimulus
 zero2: zero input beform stimulus
 batchsize: batch size for training
 device: cpu or gpu device for running
 spike_grad: surrogate delta function
-opt: optimizer
+spike_grad_approx : smooth transfer function
+optimizer: optimizer
+vthr : firing threshold
+tau_m : membrane time constant
+tau_d : synaptic decay time constant
+tau_r : synaptic rise time constant
+dt : time step
 
 """
 
 spike_grad = surrogate.fast_sigmoid()
+def spike_grad_approx(x,beta=20):
+    return (1+torch.tanh(beta*x))/2
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 input_shape=2
 output_shape=1
 batchsize=100
 n=10
-epochs_num = 3
-P=3
+epochs_num = 5
 T=500
-dt=2e-3
+P=3
+dt=0.2
 hidden_shape=100
 zero_time1=int(T/10)
 zero_time2=int(T/2)
@@ -42,4 +49,7 @@ target_zero=torch.zeros(size=(1,1)).repeat(T-zero_time2,1,1).to(device).to(torch
 zeros1=torch.zeros(size=(zero_time1,input_shape,1)).to(device)
 zeros2=torch.zeros(size=(zero_time2,input_shape,1)).to(device)
 optimizer = torch.optim.Adam
-
+vthr=torch.tensor(1.0)
+tau_m=torch.tensor(20)
+tau_d=torch.tensor(30)
+tau_r=torch.tensor(2)
